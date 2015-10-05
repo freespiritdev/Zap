78fe6e135f038b3e7e104ae5e53f63ee4f23b8bb
Cheri <avyishi@gmail.com>
2015-10-04 18:32:13 -0700

    Added carts controller and model

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,10 @@
+class CartsController < ApplicationController
+
+  def index
+    @carts = Cart.all
+  end
+
+  def new
+    @cart = Cart.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resource :products
   resource :women
+  resource :carts
 
   root to: 'welcome#index'
   
