5c320f3e9feb55330d4b991e62521b3721b8c5ff
Cheri <avyishi@gmail.com>
2015-10-07 22:46:31 -0700

    Updated woman model

--- app/models/woman.rb
+++ app/models/woman.rb
@@ -2,6 +2,9 @@ class Woman < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
 
+  def self.latest
+    Woman.order(:updated_at).last
+  end 
   
   
 end
